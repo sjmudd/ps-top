c72057ae6bd4a976a079773c0c42bedb69589541
Simon J Mudd <sjmudd@pobox.com>
2015-02-24 08:08:09 +0100

    screen: Reorder functions alphabetically and minor cleanup

--- screen/screen.go
+++ screen/screen.go
@@ -19,65 +19,6 @@ type TermboxScreen struct {
 	fg, bg        termbox.Attribute
 }
 
-type TermboxAttribute termbox.Attribute
-
-// reset the termbox to a clear screen
-func (s *TermboxScreen) Initialise() {
-	err := termbox.Init()
-	if err != nil {
-		fmt.Println("Could not start termbox for " + lib.MyName() + ". View ~/." + lib.MyName() + ".log for error messages.")
-		log.Printf("Cannot start "+lib.MyName()+", termbox.Init() gave an error:\n%s\n", err)
-		os.Exit(1)
-	}
-
-	s.Clear()
-	s.fg = termbox.ColorDefault
-	s.bg = termbox.ColorDefault
-
-	x, y := termbox.Size()
-	s.SetSize(x, y)
-}
-
-// clear the screen
-func (s *TermboxScreen) Clear() {
-	termbox.Clear(termbox.ColorWhite, termbox.ColorBlack)
-}
-
-func (s *TermboxScreen) Flush() {
-	termbox.Flush()
-}
-
-func (s *TermboxScreen) SetSize(width, height int) {
-	// if we get bigger then clear out the bottom line
-	for x := 0; x < s.width; x++ {
-		termbox.SetCell(x, s.height-1, ' ', s.fg, s.bg)
-	}
-	s.Flush()
-
-	s.width = width
-	s.height = height
-}
-
-func (s *TermboxScreen) Size() (int, int) {
-	return s.width, s.height
-}
-
-func (s *TermboxScreen) Height() int {
-	return s.height
-}
-
-// print the characters but don't print them outside the screen
-func (s *TermboxScreen) PrintAt(x int, y int, text string) {
-	offset := 0
-	for c := range text {
-		if (x + offset) < s.width {
-			termbox.SetCell(x+offset, y, rune(text[c]), s.fg, s.bg)
-			offset++
-		}
-	}
-	s.Flush()
-}
-
 // print the characters in bold (for headings) but don't print them outside the screen
 func (s *TermboxScreen) BoldPrintAt(x int, y int, text string) {
 	offset := 0
@@ -90,8 +31,19 @@ func (s *TermboxScreen) BoldPrintAt(x int, y int, text string) {
 	s.Flush()
 }
 
+// clear the screen
+func (s *TermboxScreen) Clear() {
+	termbox.Clear(termbox.ColorWhite, termbox.ColorBlack)
+}
+
+// close the screen
+func (s *TermboxScreen) Close() {
+	termbox.Close()
+}
+
+// display a help page
 func (s *TermboxScreen) DisplayHelp() {
-	s.PrintAt(0, 0, lib.MyName()+" version "+version.Version()+" (C) 2014 Simon J Mudd <sjmudd@pobox.com>")
+	s.PrintAt(0, 0, lib.MyName()+" version "+version.Version()+" "+lib.Copyright())
 
 	s.PrintAt(0, 2, "Program to show the top I/O information by accessing information from the")
 	s.PrintAt(0, 3, "performance_schema schema. Ideas based on mysql-sys.")
@@ -108,6 +60,57 @@ func (s *TermboxScreen) DisplayHelp() {
 	s.PrintAt(0, 15, "Press h to return to main screen")
 }
 
-func (s *TermboxScreen) Close() {
-	termbox.Close()
+// flush changes to screen
+func (s *TermboxScreen) Flush() {
+	termbox.Flush()
+}
+
+// return the current height of the screen
+func (s *TermboxScreen) Height() int {
+	return s.height
+}
+
+// reset the termbox to a clear screen
+func (s *TermboxScreen) Initialise() {
+	err := termbox.Init()
+	if err != nil {
+		fmt.Println("Could not start termbox for " + lib.MyName() + ". View ~/." + lib.MyName() + ".log for error messages.")
+		log.Printf("Cannot start "+lib.MyName()+", termbox.Init() gave an error:\n%s\n", err)
+		os.Exit(1)
+	}
+
+	s.Clear()
+	s.fg = termbox.ColorDefault
+	s.bg = termbox.ColorDefault
+
+	s.SetSize( termbox.Size() )
+}
+
+// print the characters but don't print them outside the screen
+func (s *TermboxScreen) PrintAt(x int, y int, text string) {
+	offset := 0
+	for c := range text {
+		if (x + offset) < s.width {
+			termbox.SetCell(x+offset, y, rune(text[c]), s.fg, s.bg)
+			offset++
+		}
+	}
+	s.Flush()
+}
+
+// set the screen size
+func (s *TermboxScreen) SetSize(width, height int) {
+	// if we get bigger then clear out the bottom line
+	for x := 0; x < s.width; x++ {
+		termbox.SetCell(x, s.height-1, ' ', s.fg, s.bg)
+	}
+	s.Flush()
+
+	s.width = width
+	s.height = height
+}
+
+// return the current (width, height) of the screen
+func (s *TermboxScreen) Size() (int, int) {
+	return s.width, s.height
 }
