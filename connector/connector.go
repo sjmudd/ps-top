a86e60b5175405aa47b6617e97c3172666bf7903
Simon J Mudd <sjmudd@pobox.com>
2015-05-20 21:21:33 +0200

    Lint/Vet fixes

--- connector/connector.go
+++ connector/connector.go
@@ -1,4 +1,4 @@
-// Use Connector to specify how to connect to MySQL.
+// Package connector is used to specify how to connect to MySQL.
 // Then get a sql.*DB from it which is returned to the app..
 package connector
 
@@ -6,50 +6,51 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql" // hide the actual driver used. Is this the right thing to do?
 
 	"github.com/sjmudd/mysql_defaults_file"
 	"github.com/sjmudd/ps-top/lib"
 )
 
 const (
-	sql_driver = "mysql"
-	db         = "performance_schema"
-
-	DEFAULTS_FILE = iota
-	COMPONENTS    = iota
+	sqlDriver    = "mysql"
+	db           = "performance_schema"
+	// ConnectByDefaultsFile indicates we want to connect using a MySQL defaults file
+	ConnectByDefaultsFile = iota
+	// ConnectByComponents indicates we want to connect by various component (fields)
+	ConnectByComponents   = iota
 )
 
-// connector struct
+// Connector contains information on how you want to connect
 type Connector struct {
 	connectBy     int
 	components    map[string]string
-	defaults_file string
+	defaultsFile string
 	dbh           *sql.DB
 }
 
-// return the database handle
+// Handle returns the database handle
 func (c Connector) Handle() *sql.DB {
 	return c.dbh
 }
 
-// return the defaults file
+// DefaultsFile returns the defaults file
 func (c Connector) DefaultsFile() string {
-	return c.defaults_file
+	return c.defaultsFile
 }
 
-// set the defaults file
-func (c *Connector) SetDefaultsFile(defaults_file string) {
-	c.defaults_file = defaults_file
+// SetDefaultsFile specifies the defaults file to use
+func (c *Connector) SetDefaultsFile(defaultsFile string) {
+	c.defaultsFile = defaultsFile
 }
 
-// set the components
+// SetComponents sets the component information needed to make the connection
 func (c *Connector) SetComponents(components map[string]string) {
 	c.components = components
 }
 
-// things to do after connecting
-func (c *Connector) postConnectStuff() {
+// postConnectAction has things to do after connecting
+func (c *Connector) postConnectAction() {
 	if err := c.dbh.Ping(); err != nil {
 		log.Fatal(err)
 	}
@@ -58,47 +59,49 @@ func (c *Connector) postConnectStuff() {
 	c.dbh.SetMaxOpenConns(5) // hard-coded value!
 }
 
-// determine how we want to connect
+// SetConnectBy records how we want to connect
 func (c *Connector) SetConnectBy(connectHow int) {
 	c.connectBy = connectHow
 }
 
-// make the database connection
+// Connect makes a connection to the database using the previously defined settings
 func (c *Connector) Connect() {
 	var err error
 
 	switch {
-	case c.connectBy == DEFAULTS_FILE:
+	case c.connectBy == ConnectByDefaultsFile:
 		lib.Logger.Println("connect_by_defaults_file() connecting to database")
 
-		c.dbh, err = mysql_defaults_file.OpenUsingDefaultsFile(sql_driver, c.defaults_file, db)
+		c.dbh, err = mysql_defaults_file.OpenUsingDefaultsFile(sqlDriver, c.defaultsFile, db)
 		if err != nil {
 			log.Fatal(err)
 		}
-	case c.connectBy == COMPONENTS:
+	case c.connectBy == ConnectByComponents:
 		lib.Logger.Println("connect_by_components() connecting to database")
 
-		new_dsn := mysql_defaults_file.BuildDSN(c.components, db)
-		c.dbh, err = sql.Open(sql_driver, new_dsn)
+		newDsn := mysql_defaults_file.BuildDSN(c.components, db)
+		c.dbh, err = sql.Open(sqlDriver, newDsn)
 		if err != nil {
 			log.Fatal(err)
 		}
 	default:
-		log.Fatal("Connector.Connect() c.connectBy not DEFAULTS_FILE/COMPONENTS")
+		log.Fatal("Connector.Connect() c.connectBy not ConnectByDefaultsFile/ConnectByComponents")
 	}
-	c.postConnectStuff()
+	c.postConnectAction()
 }
 
-// Connect to MySQL using various component parts needed to make the dsn.
+// ConnectByComponents connects to MySQL using various component
+// parts needed to make the dsn.
 func (c *Connector) ConnectByComponents(components map[string]string) {
 	c.SetComponents(components)
-	c.SetConnectBy(COMPONENTS)
+	c.SetConnectBy(ConnectByComponents)
 	c.Connect()
 }
 
-// Connect to the database with the given defaults-file, or ~/.my.cnf if not provided.
-func (c *Connector) ConnectByDefaultsFile(defaults_file string) {
-	c.SetDefaultsFile(defaults_file)
-	c.SetConnectBy(DEFAULTS_FILE)
+// ConnectByDefaultsFile connects to the database with the given
+// defaults-file, or ~/.my.cnf if not provided.
+func (c *Connector) ConnectByDefaultsFile(defaultsFile string) {
+	c.SetDefaultsFile(defaultsFile)
+	c.SetConnectBy(ConnectByDefaultsFile)
 	c.Connect()
 }
