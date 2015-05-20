521361014ca8c7139ac9761fd76408dafc6f9dd4
Simon J Mudd <sjmudd@pobox.com>
2015-05-20 20:56:57 +0200

    Lint/Vet fixes

--- version/version.go
+++ version/version.go
@@ -1,11 +1,11 @@
-// package to return the program version
+// Package version returns the version of the application(s)
 package version
 
 const (
 	version = "0.5.0"
 )
 
-// return the current application version
+// Version returns the current application version
 func Version() string {
 	return version
 }
