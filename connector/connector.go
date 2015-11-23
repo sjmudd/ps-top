af76d801d47f889312f4c6ad5ea5bc4309b8d582
Simon J Mudd <sjmudd@pobox.com>
2015-11-23 05:51:54 +0100

    Allow for collecting credentials from environment variable MYSQL_DSN
    It may be convenient to connect to different servers, but setting
    the credentials via command line parameters may not be convenient
    or secure so we use an environment variable which can be set and
    which ps-top will use.
    
    This allows you to do something like:
    
    $ MYSQL_DSN='test_user:test_pass@tcp(127.0.0.1:3306)/performance_schema' ps-top
    
    or
    
    $ export MYSQL_DSN='test_user:test_pass@tcp(127.0.0.1:3306)/performance_schema'
    $ ps-top
    
    Note: for now it's necessary to specify the database and port even if they
    are default values. I may consider fixing this later.

--- connector/connector.go
+++ connector/connector.go
@@ -1,32 +1,34 @@
 // Package connector is used to specify how to connect to MySQL.
-// Then get a sql.*DB from it which is returned to the app..
+// Then get a sql.*DB from it which is returned to the app.
 package connector
 
 import (
 	"database/sql"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql" // hide the actual driver used. Is this the right thing to do?
-
 	"github.com/sjmudd/mysql_defaults_file"
 	"github.com/sjmudd/ps-top/logger"
 )
 
 const (
-	sqlDriver = "mysql"
-	db        = "performance_schema"
+	db           = "performance_schema"
+	MaxOpenConns = 5 // hard-coded value!
+	sqlDriver    = "mysql"
+
 	// ConnectByDefaultsFile indicates we want to connect using a MySQL defaults file
 	ConnectByDefaultsFile = iota
 	// ConnectByComponents indicates we want to connect by various component (fields)
 	ConnectByComponents = iota
+	// ConnectByEnvironment indicates we want to connect by using MYSQL_DSN environment variable
+	ConnectByEnvironment = iota
 )
 
 // Connector contains information on how you want to connect
 type Connector struct {
-	connectBy    int
-	components   map[string]string
-	defaultsFile string
-	dbh          *sql.DB
+	connectMethod int
+	components    map[string]string
+	defaultsFile  string
+	dbh           *sql.DB
 }
 
 // Handle returns the database handle
@@ -51,17 +53,18 @@ func (c *Connector) SetComponents(components map[string]string) {
 
 // postConnectAction has things to do after connecting
 func (c *Connector) postConnectAction() {
+	// without calling Ping() we don't actually connect.
 	if err := c.dbh.Ping(); err != nil {
 		log.Fatal(err)
 	}
 
 	// deliberately limit the pool size to 5 to avoid "problems" if any queries hang.
-	c.dbh.SetMaxOpenConns(5) // hard-coded value!
+	c.dbh.SetMaxOpenConns(MaxOpenConns)
 }
 
 // SetConnectBy records how we want to connect
 func (c *Connector) SetConnectBy(connectHow int) {
-	c.connectBy = connectHow
+	c.connectMethod = connectHow
 }
 
 // Connect makes a connection to the database using the previously defined settings
@@ -69,23 +72,25 @@ func (c *Connector) Connect() {
 	var err error
 
 	switch {
-	case c.connectBy == ConnectByDefaultsFile:
-		logger.Println("connect_by_defaults_file() connecting to database")
-
-		c.dbh, err = mysql_defaults_file.OpenUsingDefaultsFile(sqlDriver, c.defaultsFile, db)
-		if err != nil {
-			log.Fatal(err)
-		}
-	case c.connectBy == ConnectByComponents:
+	case c.connectMethod == ConnectByComponents:
 		logger.Println("connect_by_components() connecting to database")
 
 		newDsn := mysql_defaults_file.BuildDSN(c.components, db)
 		c.dbh, err = sql.Open(sqlDriver, newDsn)
-		if err != nil {
-			log.Fatal(err)
-		}
+	case c.connectMethod == ConnectByDefaultsFile:
+		logger.Println("connect_by_defaults_file() connecting to database")
+
+		c.dbh, err = mysql_defaults_file.OpenUsingDefaultsFile(sqlDriver, c.defaultsFile, db)
+	case c.connectMethod == ConnectByEnvironment:
+		logger.Println("connect_by_environment() connecting to database")
+		c.dbh, err = mysql_defaults_file.OpenUsingEnvironment(sqlDriver)
 	default:
-		log.Fatal("Connector.Connect() c.connectBy not ConnectByDefaultsFile/ConnectByComponents")
+		log.Fatal("Connector.Connect() c.connectMethod not ConnectByDefaultsFile/ConnectByComponents")
+	}
+
+	// we catch Open...() errors here
+	if err != nil {
+		log.Fatal(err)
 	}
 	c.postConnectAction()
 }
@@ -103,5 +108,10 @@ func (c *Connector) ConnectByComponents(components map[string]string) {
 func (c *Connector) ConnectByDefaultsFile(defaultsFile string) {
 	c.SetDefaultsFile(defaultsFile)
 	c.SetConnectBy(ConnectByDefaultsFile)
+	c.Connect()
+}
+
+func (c *Connector) ConnectByEnvironment() {
+	c.SetConnectBy(ConnectByEnvironment)
 	c.Connect()
 }
--- connector/flags.go
+++ connector/flags.go
@@ -9,12 +9,13 @@ import (
 
 // Flags holds various flags related to connecting to the database
 type Flags struct {
-	Host         *string
-	Socket       *string
-	Port         *int
-	User         *string
-	Password     *string
-	DefaultsFile *string
+	Host           *string
+	Socket         *string
+	Port           *int
+	User           *string
+	Password       *string
+	DefaultsFile   *string
+	UseEnvironment *bool
 }
 
 // new connector returns a connected Connector given the different parameters
@@ -22,42 +23,46 @@ func NewConnector(flags Flags) *Connector {
 	var defaultsFile string
 	connector := new(Connector)
 
-	if *flags.Host != "" || *flags.Socket != "" {
-		logger.Println("--host= or --socket= defined")
-		var components = make(map[string]string)
-		if *flags.Host != "" && *flags.Socket != "" {
-			fmt.Println(lib.MyName() + ": Do not specify --host and --socket together")
-			os.Exit(1)
-		}
-		if *flags.Host != "" {
-			components["host"] = *flags.Host
-		}
-		if *flags.Port != 0 {
-			if *flags.Socket == "" {
-				components["port"] = fmt.Sprintf("%d", *flags.Port)
-			} else {
-				fmt.Println(lib.MyName() + ": Do not specify --socket and --port together")
+	if *flags.UseEnvironment {
+		connector.ConnectByEnvironment()
+	} else {
+		if *flags.Host != "" || *flags.Socket != "" {
+			logger.Println("--host= or --socket= defined")
+			var components = make(map[string]string)
+			if *flags.Host != "" && *flags.Socket != "" {
+				fmt.Println(lib.MyName() + ": Do not specify --host and --socket together")
 				os.Exit(1)
 			}
-		}
-		if *flags.Socket != "" {
-			components["socket"] = *flags.Socket
-		}
-		if *flags.User != "" {
-			components["user"] = *flags.User
-		}
-		if *flags.Password != "" {
-			components["password"] = *flags.Password
-		}
-		connector.ConnectByComponents(components)
-	} else {
-		if flags.DefaultsFile != nil && *flags.DefaultsFile != "" {
-			logger.Println("--defaults-file defined")
-			defaultsFile = *flags.DefaultsFile
+			if *flags.Host != "" {
+				components["host"] = *flags.Host
+			}
+			if *flags.Port != 0 {
+				if *flags.Socket == "" {
+					components["port"] = fmt.Sprintf("%d", *flags.Port)
+				} else {
+					fmt.Println(lib.MyName() + ": Do not specify --socket and --port together")
+					os.Exit(1)
+				}
+			}
+			if *flags.Socket != "" {
+				components["socket"] = *flags.Socket
+			}
+			if *flags.User != "" {
+				components["user"] = *flags.User
+			}
+			if *flags.Password != "" {
+				components["password"] = *flags.Password
+			}
+			connector.ConnectByComponents(components)
 		} else {
-			logger.Println("connecting by implicit defaults file")
+			if flags.DefaultsFile != nil && *flags.DefaultsFile != "" {
+				logger.Println("--defaults-file defined")
+				defaultsFile = *flags.DefaultsFile
+			} else {
+				logger.Println("connecting by implicit defaults file")
+			}
+			connector.ConnectByDefaultsFile(defaultsFile)
 		}
-		connector.ConnectByDefaultsFile(defaultsFile)
 	}
 
 	return connector
