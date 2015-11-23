ac4d63c078456a4b650c1a96bc1416c6b474e187
Simon J Mudd <sjmudd@pobox.com>
2015-11-23 22:34:16 +0100

    Clean up global variables by hiding functions that don't need to be visible

--- global/variables.go
+++ global/variables.go
@@ -22,16 +22,13 @@ type Variables struct {
 
 // NewVariables returns a pointer to an initialised Variables structure
 func NewVariables(dbh *sql.DB) *Variables {
-	var err error
 	if dbh == nil {
 		logger.Fatal("NewVariables(): dbh == nil")
 	}
 	v := new(Variables)
 	v.dbh = dbh
-	if v.variables, err = SelectAllVariables(dbh); err != nil {
-		logger.Fatal("NewVariables():", err)
+	v.selectAll()
 
-	}
 	return v
 }
 
@@ -47,32 +44,29 @@ func (v Variables) Get(key string) string {
 	return result
 }
 
-// SelectAllVariables returns all global variables as a hashref
-// - note the query is case insensitive for variable names.
-// - the returned keys are lower-cased.
-func SelectAllVariables(dbh *sql.DB) (map[string]string, error) {
-	logger.Println("SelectAllVariables(?)")
-
+// selectAll() collects all variables from the database and stores for later use.
+// - all returned keys are lower-cased.
+func (v *Variables) selectAll() {
 	hashref := make(map[string]string)
+
 	query := "SELECT VARIABLE_NAME, VARIABLE_VALUE FROM " + globalVariablesSchema + ".GLOBAL_VARIABLES"
 	logger.Println("query:", query)
 
-	rows, err := dbh.Query(query)
+	rows, err := v.dbh.Query(query)
 	if err != nil {
 		if (globalVariablesSchema == "INFORMATION_SCHEMA") && (err.Error() == showCompatibility56Error) {
-			logger.Println("SelectVariablesByName() I_S query failed, trying with P_S")
+			logger.Println("selectAll() I_S query failed, trying with P_S")
 			globalVariablesSchema = "PERFORMANCE_SCHEMA" // Change global variable to use P_S
 			query = "SELECT VARIABLE_NAME, VARIABLE_VALUE FROM " + globalVariablesSchema + ".GLOBAL_VARIABLES"
 			logger.Println("query:", query)
 
-			rows, err = dbh.Query(query)
+			rows, err = v.dbh.Query(query)
 		}
 		if err != nil {
-			logger.Println("SelectVariablesByName() query failed with:", err)
-			return nil, err
+			logger.Fatal("selectAll() query failed with:", err)
 		}
 	}
-	logger.Println("SelectVariablesByName() query succeeded")
+	logger.Println("selectAll() query succeeded")
 	defer rows.Close()
 
 	for rows.Next() {
@@ -85,7 +79,7 @@ func SelectAllVariables(dbh *sql.DB) (map[string]string, error) {
 	if err := rows.Err(); err != nil {
 		logger.Fatal(err)
 	}
-	logger.Println("SelectVariablesByName() result has", len(hashref), "rows")
+	logger.Println("selectAll() result has", len(hashref), "rows")
 
-	return hashref, err
+	v.variables = hashref
 }
