0f853011cbfbf2022381a09ea3f4078a89be4318
Simon J Mudd <sjmudd@pobox.com>
2017-04-08 11:21:32 +0200

    clean test case

--- lib/common_test.go
+++ lib/common_test.go
@@ -5,28 +5,9 @@ import (
 )
 
 func TestMyName(t *testing.T) {
-	if MyName() != "pstop" {
-		t.Errorf("MyName() expected to be %v but actually was %v", "pstop", MyName())
-	}
-}
-
-func Test_sec_to_time(t *testing.T) {
-	type stuff struct {
-		input  uint64
-		output string
-	}
-	testData := []stuff{
-		{0, "00:00:00"},
-		{1, "00:00:01"},
-		{61, "00:01:01"},
-		{3601, "01:00:01"},
-		{7384, "02:03:04"},
-		// add more values here
-	}
-	for i := range testData {
-		if sec_to_time(testData[i].input) != testData[i].output {
-			t.Errorf("sec_to_time(%v) expected to be %v but actually was %v", testData[i].input, testData[i].output, sec_to_time(testData[i].input))
-		}
+	const expected = "lib.test"
+	if MyName() != expected {
+		t.Errorf("MyName() expected to be %v but actually was %v", expected, MyName())
 	}
 }
 
