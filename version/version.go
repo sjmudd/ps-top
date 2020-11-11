6269c001b602fab5cce4a556aa5d243e53ad881d
Simon J Mudd <sjmudd@pobox.com>
2020-11-11 07:31:27 +0100

    Update version to 0.7.16

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 // Package version returns the version of the application
 package version
 
-const version = "0.7.9"
+const version = "0.7.16"
 
 // Version returns the current application version
 func Version() string {
