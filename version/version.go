6fd1b463013e5fd0530e8072f5d7abd815e23419
Simon J Mudd <sjmudd@pobox.com>
2020-11-16 09:01:55 +0100

    bump to version 1.0.3

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 // Package version returns the version of the application
 package version
 
-const version = "1.0.1"
+const version = "1.0.3"
 
 // Version returns the current application version
 func Version() string {
