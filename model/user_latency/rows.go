7d607e49e8a7b33c762eef4e89f90260accdd66b
Simon J Mudd <sjmudd@pobox.com>
2021-03-13 13:12:46 +0100

    simplify totals

--- model/user_latency/rows.go
+++ model/user_latency/rows.go
@@ -4,22 +4,21 @@ package user_latency
 // Rows contains a slice of Row rows
 type Rows []Row
 
-// generate a row of totals from a table
-func (t Rows) totals() Row {
-	var totals Row
-	totals.Username = "Totals"
+// totals returns the totals of all rows
+func (rows Rows) totals() Row {
+	total := Row{Username: "Totals"}
 
-	for i := range t {
-		totals.Runtime += t[i].Runtime
-		totals.Sleeptime += t[i].Sleeptime
-		totals.Connections += t[i].Connections
-		totals.Active += t[i].Active
-		totals.Selects += t[i].Selects
-		totals.Inserts += t[i].Inserts
-		totals.Updates += t[i].Updates
-		totals.Deletes += t[i].Deletes
-		totals.Other += t[i].Other
+	for _, row := range rows {
+		total.Runtime += row.Runtime
+		total.Sleeptime += row.Sleeptime
+		total.Connections += row.Connections
+		total.Active += row.Active
+		total.Selects += row.Selects
+		total.Inserts += row.Inserts
+		total.Updates += row.Updates
+		total.Deletes += row.Deletes
+		total.Other += row.Other
 	}
 
-	return totals
+	return total
 }
