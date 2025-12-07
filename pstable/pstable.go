011bdb3bb9028e67b34d648be7c08148c0bde0c7
Simon J Mudd <sjmudd@pobox.com>
2025-12-07 19:29:09 +0100

    explicitly ignore stmt.Close() return values which golinter said I should handle

--- pstable/pstable.go
+++ pstable/pstable.go
@@ -33,17 +33,17 @@ const (
 
 // Tabler is the interface for access to performance_schema rows
 type Tabler interface {
-	Collect() // Collect collects data for the table from the database
-	Description() string // return a description of the metric
-	EmptyRowContent() string // return an empty row formatted as a string
-	HaveRelativeStats() bool // do we have relative stats in the provided data?
-	Headings() string // heading for text output
+	Collect()                    // Collect collects data for the table from the database
+	Description() string         // return a description of the metric
+	EmptyRowContent() string     // return an empty row formatted as a string
+	HaveRelativeStats() bool     // do we have relative stats in the provided data?
+	Headings() string            // heading for text output
 	FirstCollectTime() time.Time // time of first collection
-	LastCollectTime() time.Time // time of last collection
-	RowContent() []string // a list of text formatted row data
-	ResetStatistics() // resets the statistics for this data
-	TotalRowContent() string // text formatted data for the "total" footer
-	WantRelativeStats() bool // do we want relative stats?
+	LastCollectTime() time.Time  // time of last collection
+	RowContent() []string        // a list of text formatted row data
+	ResetStatistics()            // resets the statistics for this data
+	TotalRowContent() string     // text formatted data for the "total" footer
+	WantRelativeStats() bool     // do we want relative stats?
 }
 
 // NewTabler returns a Tabler of the requested tablerType and parameters
