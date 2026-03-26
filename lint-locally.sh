e4ea33a3bfe9992b88ec19b8ccb7cfb371377f83
Simon J Mudd <sjmudd@pobox.com>
2026-03-26 23:23:55 +0100

    lint-locally.sh: Pin super-linter image

--- lint-locally.sh
+++ lint-locally.sh
@@ -5,4 +5,4 @@ docker run \
 	-e LOG_LEVEL=INFO \
 	-e RUN_LOCAL=true \
 	-v "$PWD":/tmp/lint \
-	ghcr.io/super-linter/super-linter:latest
+	ghcr.io/super-linter/super-linter:454ba4482ce2cd0c505bc592e83c06e1e37ade61
