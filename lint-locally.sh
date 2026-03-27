21c33a4d5d2ae739d3bc98c59cb6041fd6b888ba
Simon J Mudd <sjmudd@pobox.com>
2026-03-27 19:32:28 +0100

    disable some more linters

--- lint-locally.sh
+++ lint-locally.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+# Use a stable tag. The GitHub Action uses a different reference; the Docker image
+# is typically tagged as 'latest'. Avoid explicit hashes.
+IMAGE="ghcr.io/super-linter/super-linter:latest"
+
+# Pull the image if not present locally
+if ! docker image inspect "$IMAGE" >/dev/null 2>&1; then
+	echo "Pulling $IMAGE..."
+	docker pull "$IMAGE"
+fi
+
 docker run \
 	--rm \
 	-e LOG_LEVEL=INFO \
 	-e RUN_LOCAL=true \
+	-e VALIDATE_GO=false \
+	-e VALIDATE_GITHUB_ACTIONS_ZIZMOR=false \
+	-e VALIDATE_JSCPD=false \
+	-e VALIDATE_BIOME_FORMAT=false \
 	-v "$PWD":/tmp/lint \
-	ghcr.io/super-linter/super-linter:454ba4482ce2cd0c505bc592e83c06e1e37ade61
+	"$IMAGE"
